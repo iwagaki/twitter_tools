36f626005c9266aedccaac1f6413c055c02a1bd6
iwagaki <iwagaki@users.noreply.github.com>
2017-02-27 07:46:56 +0900

    Added a condition to delete by REPLEY_EXPIRED_TIME and STATUS_EXPIRED_TIME

--- delete_tweet_py/delete_tweet.py
+++ delete_tweet_py/delete_tweet.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+import datetime
 import random
 import urllib
 import urllib2
@@ -9,7 +10,6 @@ import base64
 import hmac
 import hashlib
 import json
-
 # References:
 # https://dev.twitter.com/oauth/overview/authorizing-requests
 
@@ -17,6 +17,9 @@ CONSUMER_KEY = os.environ.get("PROMPTWEET_CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("PROMPTWEET_CONSUMER_SECRET")
 ACCESS_TOKEN = os.environ.get("PROMPTWEET_ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("PROMPTWEET_ACCESS_TOKEN_SECRET")
+
+STATUS_EXPIRED_TIME = 60 * 30
+REPLY_EXPIRED_TIME = 60 * 60 * 24
 
 # from python-oauth2 
 # http://stackoverflow.com/questions/5590170/what-is-the-standard-method-for-generating-a-nonce-in-python
@@ -57,7 +60,6 @@ def post(method, url):
     request = urllib2.Request(url)
     if method == "POST":
         request.get_method = lambda: "POST"
-    print request.get_method()
     request.add_header("Authorization", authorization_header)
     return urllib2.urlopen(request)
     
@@ -69,7 +71,16 @@ statuses = json.loads(result.read())
 
 for status in statuses:
     status_id = status['id']
-    
-    url = "https://api.twitter.com/1.1/statuses/destroy/%s.json" % status_id
-    print url
-    print post("POST", url);
+
+    is_reply = True if status['in_reply_to_user_id'] != None else False
+    created_at = status['created_at']
+    # Sun Feb 26 21:57:11 +0000 2017
+    utc_created_at = datetime.datetime.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y")
+    utc_now = datetime.datetime.utcnow()
+    utc_delta_sec = (utc_now - utc_created_at).total_seconds()
+
+    expired_sec = REPLY_EXPIRED_TIME if is_reply else STATUS_EXPIRED_TIME
+    if utc_delta_sec > expired_sec:
+        url = "https://api.twitter.com/1.1/statuses/destroy/%s.json" % status_id
+        print url
+        print post("POST", url);
