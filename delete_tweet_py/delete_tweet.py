5b62edfa4f83eda8c732428a6dad342af254b122
iwagaki <iwagaki@users.noreply.github.com>
2017-02-27 20:39:54 +0900

    Cleaned up by autopep8

--- delete_tweet_py/delete_tweet.py
+++ delete_tweet_py/delete_tweet.py
@@ -21,22 +21,26 @@ ACCESS_TOKEN_SECRET = os.environ.get("PROMPTWEET_ACCESS_TOKEN_SECRET")
 STATUS_EXPIRED_TIME = 60 * 30
 REPLY_EXPIRED_TIME = 60 * 60 * 24
 
-# from python-oauth2 
-# http://stackoverflow.com/questions/5590170/what-is-the-standard-method-for-generating-a-nonce-in-python
-def generate_nonce(length = 8):
-    """Generate pseudorandom number."""
+
+def generate_nonce(length=8):
+    """
+    Generate pseudorandom number
+    from python-oauth2
+    http://stackoverflow.com/questions/5590170/what-is-the-standard-method-for-generating-a-nonce-in-python
+    """
     return ''.join([str(random.randint(0, 9)) for i in range(length)])
 
 
 def generate_authorization_header(values):
     header = ""
-    for (k, v) in sorted(values.items(), key = lambda x: x[0]):
+    for (k, v) in sorted(values.items(), key=lambda x: x[0]):
         header += ", " + k + "=\"" + urllib.quote_plus(v) + "\""
     header = "OAuth " + header[2:]
     return header
 
+
 def add_oauth_signature(method, url, values):
-    sorted_values = sorted(values.items(), key = lambda x: x[0])
+    sorted_values = sorted(values.items(), key=lambda x: x[0])
     text = method + "&" + urllib.quote_plus(url) + "&" + urllib.quote_plus(urllib.urlencode(sorted_values))
     key = urllib.quote_plus(CONSUMER_SECRET) + "&" + urllib.quote_plus(ACCESS_TOKEN_SECRET)
     signature = base64.b64encode(hmac.new(key, text, hashlib.sha1).digest())
@@ -46,12 +50,12 @@ def add_oauth_signature(method, url, values):
 
 def post(method, url):
     values = {
-        "oauth_consumer_key" : CONSUMER_KEY,
-        "oauth_signature_method" : "HMAC-SHA1",
-        "oauth_timestamp" : str(int(time.time())),
-        "oauth_nonce" : generate_nonce(),
-        "oauth_version" : "1.0",
-        "oauth_token" : ACCESS_TOKEN,
+        "oauth_consumer_key": CONSUMER_KEY,
+        "oauth_signature_method": "HMAC-SHA1",
+        "oauth_timestamp": str(int(time.time())),
+        "oauth_nonce": generate_nonce(),
+        "oauth_version": "1.0",
+        "oauth_token": ACCESS_TOKEN,
     }
 
     values = add_oauth_signature(method, url, values)
@@ -62,17 +66,17 @@ def post(method, url):
         request.get_method = lambda: "POST"
     request.add_header("Authorization", authorization_header)
     return urllib2.urlopen(request)
-    
+
 
 # Test
 result = post("GET", "https://api.twitter.com/1.1/statuses/user_timeline.json")
-     
+
 statuses = json.loads(result.read())
 
 for status in statuses:
     status_id = status['id']
 
-    is_reply = True if status['in_reply_to_user_id'] != None else False
+    is_reply = True if status['in_reply_to_user_id'] is not None else False
     created_at = status['created_at']
     # Sun Feb 26 21:57:11 +0000 2017
     utc_created_at = datetime.datetime.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y")
@@ -83,4 +87,4 @@ for status in statuses:
     if utc_delta_sec > expired_sec:
         url = "https://api.twitter.com/1.1/statuses/destroy/%s.json" % status_id
         print url
-        print post("POST", url);
+        print post("POST", url)
